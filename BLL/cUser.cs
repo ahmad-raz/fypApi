6f0a4e05048664bd0cfdda3c194d8ab84183f37b
ahmad-raz <raza12386@gmail.com>
2020-04-06 20:31:23 +0500

    clas

---
+++ BLL/cUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    class cUser
+    {
+        int id;
+        string FirstName;
+        string LastName;
+        string Address;
+        string City;
+        string Email;
+        string Phone;
+        string Username;
+        string Password;
+        cUserRole userRole = new cUserRole();
+
+
+    }
+}
